a2edc91ad999df79f2f984308ed582f80f42957f
jordanleven <jordanleven@me.com>
2026-04-04 16:20:07 +0000

    feat: Release v2.2.2

--- _nuxt/BAXOS5X6.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,c as r,A as t}from"./B3DUUO2L.js";const n={};function c(e,a){return s(),r("tbody",null,[t(e.$slots,"default")])}const l=Object.assign(o(n,[["render",c]]),{__name:"ProseTbody"});export{l as default};
